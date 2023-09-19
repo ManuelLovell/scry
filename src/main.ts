2198cab9591805f880bcc7c469c9a6f63ac2c03b
Manuel DeJesus <69539934+ManuelLovell@users.noreply.github.com>
2023-09-19 11:32:33 -0700

    Change player permission

--- src/main.ts
+++ src/main.ts
@@ -65,7 +65,8 @@ async function SetupForm(): Promise<void>
     const textArray = [
         "Scry! v1.1",
         "'Vanish' button temp-removes assets.",
-        "'Trash' is just another delete."];
+        "'Trash' is just another delete.",
+        "Fixed player Vanish/Delete permissions."];
 
     let currentIndex = 0;
     const textContainer = document.getElementById("bannerText")!;
@@ -154,91 +155,96 @@ async function SetupForm(): Promise<void>
                 listItem.id = "li-" + fuseItem.item.id;
                 listItem.innerHTML = `${increment}.   ${fuseItem.item.name}${trueName}${fuseItem.item.visible ? "" : " (Hidden)"}`;
 
-                const vanishButton = document.createElement('input');
-                vanishButton.type = "image";
-                vanishButton.title = "Vanish this Item";
-                vanishButton.className = "clickable";
-                vanishButton.value = "off";
-                vanishButton.onclick = async function (event: Event)
-                {
-                    event.stopPropagation();
-                    if (vanishButton.value === "off")
-                    {
-                        //await OBR.player.deselect([fuseItem.item.id]);
-
-                        // Get the item fresh from OBR, or it'll be snapshot from the search
-                        const freshItem = await OBR.scene.items.getItems([fuseItem.item.id]);
-                        if (freshItem.length > 0)
-                        {
-                            storedItems.push(freshItem[0]);
-                            await OBR.scene.items.deleteItems([fuseItem.item.id]);
-                            vanishButton.value = "on";
-                            vanishButton.src = "/eyeclosed.svg";
-
-                            try
-                            {
-                                const json = JSON.stringify(storedItems);
-                                localStorage.setItem(Constants.SCRYKEY, json);
-                            }
-                            catch (error)
-                            {
-                                console.log("Cannot save Vanished Items to localstorage, may lose on refresh");
-                            }
-                        }
-
-                    }
-                    else
-                    {
-                        const freshlyStored = storedItems.find(x => x.id === fuseItem.item.id);
-                        if (freshlyStored)
-                        {
-                            delete freshlyStored.customTextName;
-
-                            await OBR.scene.items.addItems([freshlyStored]);
-                            storedItems = storedItems.filter(x => x.id !== fuseItem.item.id);
-                            vanishButton.value = "off";
-                            vanishButton.src = "/eyeopen.svg";
-
-                            try
-                            {
-                                const json = JSON.stringify(storedItems);
-                                localStorage.setItem(Constants.SCRYKEY, json);
-                            }
-                            catch (error)
-                            {
-                                console.log("Cannot save Vanished Items to localstorage, may lose on refresh");
-                            }
-                        }
-                    }
-                };
-                vanishButton.src = "/eyeopen.svg";
-
-                const trashButton = document.createElement('input');
-                trashButton.type = "image";
-                trashButton.title = "Delete this Item";
-                trashButton.className = "clickable";
-                trashButton.src = "/trash.svg";
-                trashButton.onclick = async function (event: Event)
-                {
-                    event.stopPropagation();
-                    await OBR.scene.items.deleteItems([fuseItem.item.id]);
-                    storedItems = storedItems.filter(x => x.id !== fuseItem.item.id);
-                    try
-                    {
-                        const json = JSON.stringify(storedItems);
-                        localStorage.setItem(Constants.SCRYKEY, json);
-                    }
-                    catch (error)
-                    {
-                        console.log("Cannot save Vanished Items to localstorage, may lose on refresh");
-                    }
-                    listItem.remove();
-                    countElement.innerText = (+countElement.innerText - 1).toString();
-                }
                 const div = document.createElement('div');
                 div.className = "listdiv";
-                div.appendChild(trashButton);
-                div.appendChild(vanishButton);
+
+                if (role === "GM")
+                {
+                    const vanishButton = document.createElement('input');
+                    vanishButton.type = "image";
+                    vanishButton.title = "Vanish this Item";
+                    vanishButton.className = "clickable";
+                    vanishButton.value = "off";
+                    vanishButton.onclick = async function (event: Event)
+                    {
+                        event.stopPropagation();
+                        if (vanishButton.value === "off")
+                        {
+                            //await OBR.player.deselect([fuseItem.item.id]);
+
+                            // Get the item fresh from OBR, or it'll be snapshot from the search
+                            const freshItem = await OBR.scene.items.getItems([fuseItem.item.id]);
+                            if (freshItem.length > 0)
+                            {
+                                storedItems.push(freshItem[0]);
+                                await OBR.scene.items.deleteItems([fuseItem.item.id]);
+                                vanishButton.value = "on";
+                                vanishButton.src = "/eyeclosed.svg";
+
+                                try
+                                {
+                                    const json = JSON.stringify(storedItems);
+                                    localStorage.setItem(Constants.SCRYKEY, json);
+                                }
+                                catch (error)
+                                {
+                                    console.log("Cannot save Vanished Items to localstorage, may lose on refresh");
+                                }
+                            }
+
+                        }
+                        else
+                        {
+                            const freshlyStored = storedItems.find(x => x.id === fuseItem.item.id);
+                            if (freshlyStored)
+                            {
+                                delete freshlyStored.customTextName;
+
+                                await OBR.scene.items.addItems([freshlyStored]);
+                                storedItems = storedItems.filter(x => x.id !== fuseItem.item.id);
+                                vanishButton.value = "off";
+                                vanishButton.src = "/eyeopen.svg";
+
+                                try
+                                {
+                                    const json = JSON.stringify(storedItems);
+                                    localStorage.setItem(Constants.SCRYKEY, json);
+                                }
+                                catch (error)
+                                {
+                                    console.log("Cannot save Vanished Items to localstorage, may lose on refresh");
+                                }
+                            }
+                        }
+                    };
+                    vanishButton.src = "/eyeopen.svg";
+
+                    const trashButton = document.createElement('input');
+                    trashButton.type = "image";
+                    trashButton.title = "Delete this Item";
+                    trashButton.className = "clickable";
+                    trashButton.src = "/trash.svg";
+                    trashButton.onclick = async function (event: Event)
+                    {
+                        event.stopPropagation();
+                        await OBR.scene.items.deleteItems([fuseItem.item.id]);
+                        storedItems = storedItems.filter(x => x.id !== fuseItem.item.id);
+                        try
+                        {
+                            const json = JSON.stringify(storedItems);
+                            localStorage.setItem(Constants.SCRYKEY, json);
+                        }
+                        catch (error)
+                        {
+                            console.log("Cannot save Vanished Items to localstorage, may lose on refresh");
+                        }
+                        listItem.remove();
+                        countElement.innerText = (+countElement.innerText - 1).toString();
+                    }
+                    div.appendChild(trashButton);
+                    div.appendChild(vanishButton);
+                }
+
                 listItem.appendChild(div);
 
                 listItem.onclick = async () =>
@@ -251,63 +257,43 @@ async function SetupForm(): Promise<void>
             }
         }
 
-        // For the vanished cache
-        const vFuse = new Fuse(storedItems, { threshold: 0.4, includeScore: true, keys: ['name', 'customTextName'] });
-        const vFoundItems = vFuse.search(term);
-
-        if (vFoundItems.length > 0)
+        if (role === "GM")
         {
-            const visibleCount = role === "GM" ? vFoundItems.length : vFoundItems.filter(item => item.item.visible === true).length;
-            countElement.innerText = foundBase > 0 ? (foundBase + visibleCount).toString() : visibleCount.toString();
+            // For the vanished cache
+            const vFuse = new Fuse(storedItems, { threshold: 0.4, includeScore: true, keys: ['name', 'customTextName'] });
+            const vFoundItems = vFuse.search(term);
 
-            for (const vFuseItem of vFoundItems)
+            if (vFoundItems.length > 0)
             {
-                if (vFuseItem.item.visible === false && role !== "GM") continue;
+                const visibleCount = role === "GM" ? vFoundItems.length : vFoundItems.filter(item => item.item.visible === true).length;
+                countElement.innerText = foundBase > 0 ? (foundBase + visibleCount).toString() : visibleCount.toString();
 
-                const listItem = document.createElement("li");
-                let trueName = vFuseItem.item.name.toUpperCase().includes(term) ? "" : ` (${vFuseItem.item.text?.plainText})`;
-                if (trueName == " ()") trueName = "";
-
-                listItem.id = "li-" + vFuseItem.item.id;
-                listItem.innerHTML = `${increment}.   ${vFuseItem.item.name}${trueName}${vFuseItem.item.visible ? "" : " (Hidden)"}`;
-
-                const vanishButton = document.createElement('input');
-                vanishButton.type = "image";
-                vanishButton.title = "Restore this Item";
-                vanishButton.className = "clickable";
-                vanishButton.value = "off";
-                vanishButton.onclick = async function (event: Event)
+                for (const vFuseItem of vFoundItems)
                 {
-                    event.stopPropagation();
-                    if (vanishButton.value === "off")
-                    {
-                        delete vFuseItem.item.customTextName;
-                        await OBR.scene.items.addItems([vFuseItem.item]);
-                        storedItems = storedItems.filter(x => x.id !== vFuseItem.item.id);
-                        vanishButton.src = "/eyeopen.svg";
-                        vanishButton.value = "on";
+                    if (vFuseItem.item.visible === false && role !== "GM") continue;
 
-                        try
-                        {
-                            const json = JSON.stringify(storedItems);
-                            localStorage.setItem(Constants.SCRYKEY, json);
-                        }
-                        catch (error)
-                        {
-                            console.log("Cannot save Vanished Items to localstorage, may lose on refresh");
-                        }
-                    }
-                    else
+                    const listItem = document.createElement("li");
+                    let trueName = vFuseItem.item.name.toUpperCase().includes(term) ? "" : ` (${vFuseItem.item.text?.plainText})`;
+                    if (trueName == " ()") trueName = "";
+
+                    listItem.id = "li-" + vFuseItem.item.id;
+                    listItem.innerHTML = `${increment}.   ${vFuseItem.item.name}${trueName}${vFuseItem.item.visible ? "" : " (Hidden)"}`;
+
+                    const vanishButton = document.createElement('input');
+                    vanishButton.type = "image";
+                    vanishButton.title = "Restore this Item";
+                    vanishButton.className = "clickable";
+                    vanishButton.value = "off";
+                    vanishButton.onclick = async function (event: Event)
                     {
-                        // Get the item fresh from OBR, or it'll be snapshot from the search
-                        const freshItem = await OBR.scene.items.getItems([vFuseItem.item.id]);
-                        if (freshItem.length > 0)
+                        event.stopPropagation();
+                        if (vanishButton.value === "off")
                         {
-                            storedItems.push(freshItem[0]);
-                            //await OBR.player.deselect([vFuseItem.item.id]);
-                            await OBR.scene.items.deleteItems([vFuseItem.item.id]);
-                            vanishButton.value = "off";
-                            vanishButton.src = "/eyeclosed.svg";
+                            delete vFuseItem.item.customTextName;
+                            await OBR.scene.items.addItems([vFuseItem.item]);
+                            storedItems = storedItems.filter(x => x.id !== vFuseItem.item.id);
+                            vanishButton.src = "/eyeopen.svg";
+                            vanishButton.value = "on";
 
                             try
                             {
@@ -319,45 +305,68 @@ async function SetupForm(): Promise<void>
                                 console.log("Cannot save Vanished Items to localstorage, may lose on refresh");
                             }
                         }
-                    }
-                };
-                vanishButton.src = "/eyeclosed.svg";
+                        else
+                        {
+                            // Get the item fresh from OBR, or it'll be snapshot from the search
+                            const freshItem = await OBR.scene.items.getItems([vFuseItem.item.id]);
+                            if (freshItem.length > 0)
+                            {
+                                storedItems.push(freshItem[0]);
+                                //await OBR.player.deselect([vFuseItem.item.id]);
+                                await OBR.scene.items.deleteItems([vFuseItem.item.id]);
+                                vanishButton.value = "off";
+                                vanishButton.src = "/eyeclosed.svg";
 
-                const trashButton = document.createElement('input');
-                trashButton.type = "image";
-                trashButton.title = "Delete this Item";
-                trashButton.className = "clickable";
-                trashButton.src = "/trash.svg";
-                trashButton.onclick = async function (event: Event)
-                {
-                    event.stopPropagation();
-                    await OBR.scene.items.deleteItems([vFuseItem.item.id]);
-                    storedItems = storedItems.filter(x => x.id !== vFuseItem.item.id);
-                    try
+                                try
+                                {
+                                    const json = JSON.stringify(storedItems);
+                                    localStorage.setItem(Constants.SCRYKEY, json);
+                                }
+                                catch (error)
+                                {
+                                    console.log("Cannot save Vanished Items to localstorage, may lose on refresh");
+                                }
+                            }
+                        }
+                    };
+                    vanishButton.src = "/eyeclosed.svg";
+
+                    const trashButton = document.createElement('input');
+                    trashButton.type = "image";
+                    trashButton.title = "Delete this Item";
+                    trashButton.className = "clickable";
+                    trashButton.src = "/trash.svg";
+                    trashButton.onclick = async function (event: Event)
                     {
-                        const json = JSON.stringify(storedItems);
-                        localStorage.setItem(Constants.SCRYKEY, json);
+                        event.stopPropagation();
+                        await OBR.scene.items.deleteItems([vFuseItem.item.id]);
+                        storedItems = storedItems.filter(x => x.id !== vFuseItem.item.id);
+                        try
+                        {
+                            const json = JSON.stringify(storedItems);
+                            localStorage.setItem(Constants.SCRYKEY, json);
+                        }
+                        catch (error)
+                        {
+                            console.log("Cannot save Vanished Items to localstorage, may lose on refresh");
+                        }
+                        listItem.remove();
+                        countElement.innerText = (+countElement.innerText - 1).toString();
                     }
-                    catch (error)
+                    const div = document.createElement('div');
+                    div.className = "listdiv";
+                    div.appendChild(trashButton);
+                    div.appendChild(vanishButton);
+                    listItem.appendChild(div);
+
+                    listItem.onclick = async () =>
                     {
-                        console.log("Cannot save Vanished Items to localstorage, may lose on refresh");
-                    }
-                    listItem.remove();
-                    countElement.innerText = (+countElement.innerText - 1).toString();
+                        ViewportFunctions.CenterViewportOnImage(vFuseItem.item);
+                        await OBR.player.select([vFuseItem.item.id]);
+                    };
+                    searchList.appendChild(listItem);
+                    increment++;
                 }
-                const div = document.createElement('div');
-                div.className = "listdiv";
-                div.appendChild(trashButton);
-                div.appendChild(vanishButton);
-                listItem.appendChild(div);
-
-                listItem.onclick = async () =>
-                {
-                    ViewportFunctions.CenterViewportOnImage(vFuseItem.item);
-                    await OBR.player.select([vFuseItem.item.id]);
-                };
-                searchList.appendChild(listItem);
-                increment++;
             }
         }
     }
