6fe49cd65659815c0f8f21138c1de9ca0beaa86d
ManuelLovell <69539934+ManuelLovell@users.noreply.github.com>
2024-04-24 14:53:44 +0000

    Build: (db09e04) StoredDataFix

--- assets/whatsnew-a2c7d9ca.js
+++ assets/whatsnew-a2c7d9ca.js
@@ -1,5 +1,8 @@
-import{O as o,C as i}from"./constants-b44d1cff.js";const c=document.querySelector("#bs-whatsnew"),d=document.querySelector("#bs-whatsnew-notes");c.innerHTML=`
+import{O as o,C as a}from"./constants-fee10c21.js";const c=document.querySelector("#bs-whatsnew"),d=document.querySelector("#bs-whatsnew-notes");c.innerHTML=`
   <div id="newsContainer">
+    <h1>Scry! 4/24</h1>
+    Slight hiccup. Forgot to add the loader for stored 'Vanisihed' items. Fixed.
+    </br>
     <h1>Scry! 4/23</h1>
     Slight revamp. Bringing things more in line with the other extensions.
     </br> Next steps will be adding more documentation to this part (As well as the OBR store..) but as you can guess, I have a lot of writing to do.
@@ -19,4 +22,4 @@ import{O as o,C as i}from"./constants-b44d1cff.js";const c=document.querySelecto
             ${e?'<embed id="patreonLogo" class="svg thankyou" src="/thankyou.svg" />':'<embed id="patreonLogo" class="svg patreon" src="/w-patreon.png" />'}</button>
         </div>
         <button id="closeButton" type="button" title="Close this window"><embed class="svg close" src="/w-close.svg" /></button>
-        `;const s=document.getElementById("closeButton");s.onclick=async()=>{await o.modal.close(i.EXTENSIONWHATSNEW)};const r=document.getElementById("discordButton");r.onclick=async t=>{t.preventDefault(),window.open("https://discord.gg/ANZKDmWzr6","_blank")};const a=document.getElementById("patreonButton");a.onclick=async t=>{t.preventDefault(),window.open("https://www.patreon.com/battlesystem","_blank")}});
+        `;const s=document.getElementById("closeButton");s.onclick=async()=>{await o.modal.close(a.EXTENSIONWHATSNEW)};const r=document.getElementById("discordButton");r.onclick=async t=>{t.preventDefault(),window.open("https://discord.gg/ANZKDmWzr6","_blank")};const i=document.getElementById("patreonButton");i.onclick=async t=>{t.preventDefault(),window.open("https://www.patreon.com/battlesystem","_blank")}});
